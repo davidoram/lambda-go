df4b91e2487013d6c0e4a9ea28e1888e8c702bd2
David Oram <david.oram99@gmail.com>
2022-04-07 14:14:45 +1200

    Bye bye

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -11,8 +12,19 @@ import (
 // It uses Amazon API Gateway request/responses provided by the aws-lambda-go/events package,
 // However you could use other event sources (S3, Kinesis etc), or JSON-decoded primitive types such as 'string'.
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	// stdout and stderr are sent to AWS CloudWatch Logs
+
 	log.Printf("Processing Lambda request %t\n", request.RequestContext)
+	log.Printf("Path %s\n", request.Path)
+
+	if strings.HasSuffix(request.Path, "byebye") {
+		// stdout and stderr are sent to AWS CloudWatch Logs
+		return events.APIGatewayProxyResponse{
+			Body:       "Bye Bye",
+			StatusCode: 200,
+		}, nil
+	}
+
+	// stdout and stderr are sent to AWS CloudWatch Logs
 	return events.APIGatewayProxyResponse{
 		Body:       "Hello world",
 		StatusCode: 200,
